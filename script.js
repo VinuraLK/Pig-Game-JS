ed6712954da39fba7ad805da9e52413920be1055
Vinura Deshapriya <vinura@Vinuras-MacBook-Air.local>
2023-01-11 03:02:18 +0530

    added functionality for holding dice and winning if score is greater than 100

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const btnHold = document.querySelector(".btn--hold");
 
 let activePlayer = 0;
 let scores = [0, 0];
+let playing = true;
 
 let currentScore = 0;
 // Starting conditions
@@ -21,37 +22,59 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
+    // 2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    console.log(dice);
 
-  // 3. Check for rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to the next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
 
 // Holding the score
 
 btnHold.addEventListener("click", function () {
-  // 1. Add current score to active  player's score
-  // 2. Check if players score is >= 100
-  // Finish the game
-  // Switch to the next player
+  if (playing) {
+    // 1. Add current score to active  player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if players score is >= 100
+    if (scores[activePlayer] > 20) {
+      playing = false;
+      // Finish the game
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+    }
+
+    // Switch to the next player
+    switchPlayer();
+  }
 });
